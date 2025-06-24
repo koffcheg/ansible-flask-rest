53894a4f3c0dd816abb359c91430d7f02f09ce05
koffcheg <kharkov501@gmail.com>
2025-06-25 02:59:59 +0300

    update Auth

--- config.py
+++ config.py
@@ -8,3 +8,4 @@ class Config:
     ANSIBLE_PLAYBOOK_PATH = os.getenv("ANSIBLE_PLAYBOOK_PATH")
     ANSIBLE_BIN = os.getenv("ANSIBLE_BIN")
     ANSIBLE_CWD= os.getenv('ANSIBLE_CWD')
+    FACTORY_OUTPUT_DIR = os.getenv("FACTORY_OUTPUT_DIR")
