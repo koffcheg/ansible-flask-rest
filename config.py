6ba0c0c3365aa5b0f3f578d5ec2523432d1cac5d
koffcheg <kharkov501@gmail.com>
2025-06-24 22:23:21 +0300

    update Auth

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ load_dotenv()
 class Config:
     API_AUTH_TOKEN = os.getenv("API_AUTH_TOKEN")
     ANSIBLE_PLAYBOOK_PATH = os.getenv("ANSIBLE_PLAYBOOK_PATH")
-    ANSIBLE_VENV_PATH = os.getenv("ANSIBLE_VENV_PATH")
+    ANSIBLE_BIN = os.getenv("ANSIBLE_BIN")
